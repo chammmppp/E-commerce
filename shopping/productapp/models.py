39d5ff0b3da69ab4983562ec2253771a9433e7e0
Chammmppp <champ.thichanon@gmail.com>
2024-06-04 17:08:00 +0700

    edit some file

--- shopping/productapp/models.py
+++ shopping/productapp/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     isTrending = models.BooleanField(default=False)
     image = models.ImageField(upload_to="product", blank=True)
-    
--- shopping/productapp/urls.py
+++ shopping/productapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from productapp import views
 
 urlpatterns=[
-    path("product",views.index)
+    path("",views.index)
 ]
